11fdda65d8d352bf4010103f96a07b6521b02e6d
Mateus <mateusmarques51@gmail.com>
2021-09-07 16:52:55 -0300

    Aula 2

---
+++ Js/main.js
@@ -0,0 +1,15 @@
+//alert("Meu primeiro Js");
+var lista = ["maça", "Pera", "Laranja"]; //Lista
+//lista.push("Uva");                        Adiciona Uva a lista
+//lista.pop();                              Remove o ultimo elemento da lista
+//console.log(lista.length);                Retorna o tamanho da lista
+//console.log(lista.reverse());             Retorna os elementos da lista na ordem contrária
+console.log(lista);
+
+var fruta = {nome:"maça", cor:"vermelha"} //Dicionário
+console.log(fruta);
+console.log(fruta.nome);
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]; //Lista de dicionários
+console.log(frutas);
+console.log(frutas[1].nome);
